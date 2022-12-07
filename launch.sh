c1cfd4f0005c2c0d31099dd00fc5845c849b3de5
TheGrella <thegrella@gmail.com>
2022-12-07 09:15:00 +0100

    [ADD] last query

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PYSPARK_DRIVER_PYTHON=python
-export PYSPARK_PYTHON=$HOME/anaconda3/envs/tugrafa/bin/python
-spark-submit \
-    --packages com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 \
-    --archives environment.tar.gz#environment \
-    --py-files path.py,pyspark_kmodes.py,Kmodes.py,graph.py \
-    main.py
