dfd43437e4c0c4589b85c6aab4416b5df94da87b
Filippo Ziche <filippo.ziche@gmail.com>
2022-12-14 17:19:23 +0100

    cleared repo of old files

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export PYSPARK_DRIVER_PYTHON=python3
-export PYSPARK_PYTHON=/usr/bin/python3
+export PYSPARK_PYTHON=/home/filippo/anaconda3/envs/tugrafa/bin/python3.8
 spark-submit \
     --packages com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 \
     --py-files path.py,pyspark_kmodes.py,Kmodes.py,graph.py,deviation.py \
-    main.py
+    tugrafa.py
     
 #--archives environment.tar.gz#environment \
--- references.md
+++
@@ -1,3 +0,0 @@
-# Collection of usefull resources
-
-https://towardsdatascience.com/spark-graphx-pregel-its-not-so-complex-as-it-sounds-d196da246c73
